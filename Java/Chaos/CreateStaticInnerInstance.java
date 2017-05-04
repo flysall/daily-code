6b4528c8bbd6567bb9855fb587415599375b0b36
flysall <flysall@163.com>
2017-05-04 22:29:44 +0800

    add some chaos code in java

---
+++ Java/Chaos/CreateStaticInnerInstance.java
@@ -0,0 +1,7 @@
+
+public class CreateStaticInnerInstance {
+	public static void main(String[] args)
+	{
+		StaticOut.StaticIn in = new StaticOut.StaticIn();
+	}
+}
---
+++ Java/Chaos/SubClass.java
@@ -0,0 +1,6 @@
+
+public class SubClass extends Out.In {
+	public SubClass(Out out) {
+		out.super("hello");
+	}
+}
