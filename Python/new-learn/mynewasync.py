7238e1fbfc4b5364404f25076290cbdee835402a
flysall <flysallme@gmail.com>
2017-08-25 10:10:18 +0800

    Python: finish all

---
+++ Python/new-learn/mynewasync.py
@@ -0,0 +1,10 @@
+import asyncio
+async def hello():
+	print("Hello world!")
+	r = await asyncio.sleep(5)
+	print("Hello again")
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close();	
