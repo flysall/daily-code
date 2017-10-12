8f5d79ab8ae7d587e3396484eed40db845623eab
flysall <flysallme@gmail.com>
2017-10-12 22:44:46 +0800

    springboot: build

---
+++ Java-EE/springboot-demo/spring-boot-build/src/main/java/org/springframework/boot/springbootbuild/SpringBootBuildApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.boot.springbootbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuildApplication.class, args);
+	}
+}
