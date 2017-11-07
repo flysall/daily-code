d396b6015f53dc03a5101f25b845fcff1cfd2356
flysall <flysallme@gmail.com>
2017-11-07 22:38:25 +0800

    'springboot:'

---
+++ Java/EE/springboot-demo/spring-boot/spring-boot-samples/spring-boot-sample-web-freemarker/src/main/java/smaple/freemarker/SampleWebFreeMarkerApplication.java
@@ -0,0 +1,11 @@
+package smaple.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebFreeMarkerApplication {
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(SampleWebFreeMarkerApplication.class, args);
+	}
+}
---
+++ Java/EE/springboot-demo/spring-boot/spring-boot-samples/spring-boot-sample-web-freemarker/src/main/java/smaple/freemarker/WelcomeController.java
@@ -0,0 +1,20 @@
+package smaple.freemarker;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	@Value("${application.message: Hello Doctor and flysall}")
+	private String message = "Hello Doctor";
+
+	@GetMapping("/")
+	public String welcome(Map<String, Object> model){
+		model.put("time", new Date());
+		model.put("message", this.message);
+		return "welcome";
+	}
+}
