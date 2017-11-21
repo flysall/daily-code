d1ea198c3327f38e6fd959c019cad0ed36ba40c0
flysall <flysall@163.com>
2017-11-21 20:55:18 +0800

    flysall

---
+++ Java/EE/springboot-demo/spring-boot/spring-boot-samples/spring-boot-sample-web-ui/src/main/java/sample/web/ui/MessageRepository.java
@@ -0,0 +1,11 @@
+package sample.web.ui;
+
+public interface MessageRepository {
+    Iterable<Message> findAll();
+
+    Message save(Message message);
+
+    Message findMessage(Long id);
+
+    void deleteMessage(Long id);
+}
