ef073af9d5eaeb96e8d7252133b7e25c76790165
flysall <flysallme@gmail.com>
2018-01-16 08:37:12 +0800

    Think-initialization-2018-1-16

---
+++ Java/ThinkingInJava/src/flysall/initialization/ArrayNew.java
@@ -0,0 +1,13 @@
+package flysall.initialization;
+
+import java.util.*;
+
+public class ArrayNew {
+    public static void main(String[] args){
+        int[] a;
+        Random rand = new Random(47);
+        a = new int[rand.nextInt(20)];
+        System.out.println("length of a = " + a.length);
+        System.out.println(Arrays.toString(a));
+    }
+}
