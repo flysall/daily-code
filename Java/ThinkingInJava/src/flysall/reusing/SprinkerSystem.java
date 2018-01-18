3232aa0e3776b78d859ab7baeea667411f77c29e
flysall <flysallme@gmail.com>
2018-01-18 15:22:58 +0800

    Think-reusing-2018-1-18

---
+++ Java/ThinkingInJava/src/flysall/reusing/SprinkerSystem.java
@@ -0,0 +1,31 @@
+package flysall.reusing;
+
+class WaterSource {
+    private String s;
+    WaterSource() {
+        System.out.println("WaterSource()");
+        s = "Constructed";
+    }
+    public String toString() {
+        return s;
+    }
+}
+public class SprinkerSystem {
+    private String valve1, valve2, valve3, valve4;
+    private WaterSource source = new WaterSource();
+    private int i;
+    private float f;
+    public String toString() {
+        return
+                "valve1 = " + valve1 + " " +
+                        "valve2 = " + valve2 + " " +
+                        "valve3 = " + valve3 + " " +
+                        "valve4 = " + valve4 + "\n" +
+                        "i = " + i + " " + "f = " + f + " " +
+                        "source = " + source;
+    }
+    public static void main(String[] args){
+        SprinkerSystem sprinkers = new SprinkerSystem();
+        System.out.println(sprinkers);
+    }
+}
