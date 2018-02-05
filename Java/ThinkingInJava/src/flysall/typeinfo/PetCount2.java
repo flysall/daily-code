bd27c52f70c9c94fe9dffcc906569ada4970a9a9
flysall <flysallme@gmail.com>
2018-02-05 09:23:31 +0800

    Think-2018-2-5

---
+++ Java/ThinkingInJava/src/flysall/typeinfo/PetCount2.java
@@ -0,0 +1,9 @@
+package flysall.typeinfo;
+
+import flysall.typeinfo.pets.Pets;
+
+public class PetCount2 {
+    public static void main(String[] args) {
+        PetCount.countPets(Pets.creator);
+    }
+}
